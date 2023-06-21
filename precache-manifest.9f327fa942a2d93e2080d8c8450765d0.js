9fcc743cdb5f9063703a935fa3accdd736ee8e4e
Aveta Vos <avetavos@hotmail.com>
2023-06-22 00:58:04 +0700

    Updates

---
+++ precache-manifest.9f327fa942a2d93e2080d8c8450765d0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "33e4efddfcae8017d30c",
+    "url": "/static/css/main.363ec741.chunk.css"
+  },
+  {
+    "revision": "33e4efddfcae8017d30c",
+    "url": "/static/js/main.33e4efdd.chunk.js"
+  },
+  {
+    "revision": "c0dba26f4ccf71cea450",
+    "url": "/static/js/1.c0dba26f.chunk.js"
+  },
+  {
+    "revision": "d0691d964f1c3f64ebdb",
+    "url": "/static/js/2.d0691d96.chunk.js"
+  },
+  {
+    "revision": "561708c8e89dc260303a",
+    "url": "/static/js/runtime~main.561708c8.js"
+  },
+  {
+    "revision": "125f3e1be14d433a5f0f243d623f6376",
+    "url": "/static/media/project-thumbnail.125f3e1b.jpg"
+  },
+  {
+    "revision": "b3f54039b4ed0f80ca63467fb7eb6a0f",
+    "url": "/static/media/avatar.b3f54039.png"
+  },
+  {
+    "revision": "c33dfdb7e6a5a6c83e403c21327ce2cc",
+    "url": "/static/media/footer-mobile.c33dfdb7.png"
+  },
+  {
+    "revision": "0eec129e9ccea24e283ca209fe30fcd5",
+    "url": "/static/media/about-bg.0eec129e.png"
+  },
+  {
+    "revision": "b4b7dcb31ceff50245970720a32491b2",
+    "url": "/static/media/profile.b4b7dcb3.jpg"
+  },
+  {
+    "revision": "3424e74e3b64b20959b3bc97c1a3a3f8",
+    "url": "/index.html"
+  }
+];
